1cc6549c11aaebbc21775b4bcbd53be3ea56f589
Raju kumar <85837020+RajuKumar077@users.noreply.github.com>
2025-03-06 22:15:43 +0530

    Add files via upload

---
+++ packageDay1/EvenorOdd.java
@@ -0,0 +1,12 @@
+package packageDay1;
+
+public class EvenorOdd {
+    public static void main(String[] args) {
+        int num = 4;
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
---
+++ packageDay1/PrimeCheck.java
@@ -0,0 +1,19 @@
+package packageDay1;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num = 11;
+        boolean isPrime = true;
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println(num + " is Prime");
+        } else {
+            System.out.println(num + " is not Prime");
+        }
+    }
+}
